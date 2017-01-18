479293e3aa4bc28c5b9a76a76c5355547b7857ef
creedr <crr0929@gmail.com>
2017-01-18 09:54:36 -0600

    adding CNAME for custom domain name

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,5 +33,9 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
+# use custom domain name
+STATIC_PATHS = ['extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, }
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
